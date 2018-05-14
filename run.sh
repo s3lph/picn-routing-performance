c8e779160976b4c9c9918db89bc84489ffeeb910
s3lph <5564491+s3lph@users.noreply.github.com>
2018-05-15 00:14:01 +0200

    Re-enabled full run

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 ulimit -t 60
 
-for n in `seq 1 1`; do
+for n in `seq 1 10`; do
   for interval in 0.5 1.0 2.0; do
-    for run in `seq 1 5`; do
+    for run in `seq 1 20`; do
         env PYTHONPATH="$(realpath ../picn)" python3.6 picn-routing-measurements.py $n $run $interval
     done
   done
