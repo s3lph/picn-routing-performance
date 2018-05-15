0618bdd347f788a27477affc2f4d70b0b3dee0ac
s3lph <5564491+s3lph@users.noreply.github.com>
2018-05-15 20:32:20 +0200

    Real time constraints rather than cpu time constraints.

--- run.sh
+++ run.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
 
-ulimit -t 60
-
 for n in `seq 1 10`; do
   for interval in 0.5 1.0 2.0; do
     for run in `seq 1 20`; do
-        env PYTHONPATH="$(realpath ./picn)" python3.6 picn-routing-measurements.py $n $run $interval
+        timeout -k 5s -s TERM "$((10+5*$n))s" env PYTHONPATH="$(realpath ./picn)" python3.6 picn-routing-measurements.py $n $interval $run
     done
   done
 done
-
-ulimit -t 0
